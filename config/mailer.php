f3935a349602b07171ae1ffcd94d852c0b493ffb
Xavier <xvr.cello@gmail.com>
2018-03-06 14:46:37 +0100

    Delete mailer.php

--- config/mailer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'smtp'     => 'smtp.orange.fr',
-    'username' => 'coutant.xavier@orange.fr',
-    'password' => 'marseille2',
-    'from'     => 'xavierus70@hotmail.com',
-    'to'       => 'coutant.xavier@orange.fr'
-];
