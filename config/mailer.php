814d2e5c4e54cb8f235cbbe5bfb8831f97959e1d
Xavier <xavierus70@hotmail.com>
2020-03-09 21:59:38 +0100

    Update mailer.php
    suppression data mail

--- config/mailer.php
+++ config/mailer.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'smtp'     => 'fajitas.o2switch.net',
-    'username' => 'contact@devxdemo.eu',
-    'password' => 'DovL9*zyQvs0',
-    'from'     => 'contact@devxdemo.eu',
-    'to'       => 'coutant.xavier@orange.fr'
+    'smtp'     => '**********',
+    'username' => '*********',
+    'password' => '**********',
+    'from'     => '**********',
+    'to'       => '***********'
 ];
